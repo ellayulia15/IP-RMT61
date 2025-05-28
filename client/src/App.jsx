2ad4962e7b5b79f9f75493b677e32bb16394d2f6
ellayulia15 <ella.yuliacahyani@gmail.com>
2025-05-28 13:49:49 +0700

    Add EditProfile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,64 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import Home from "./pages/Home";
 import Tutors from "./pages/Tutors";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import DashboardTutor from "./pages/DashboardTutor";
 import CreateProfile from "./pages/CreateProfile";
+import EditProfile from "./pages/EditProfile";
+
+// Auth protection component
+function RequireAuth({ children }) {
+  const token = localStorage.getItem('access_token');
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+}
+
+// Role protection component
+function RequireTutor({ children }) {
+  const role = localStorage.getItem('user_role');
+  if (role !== 'Tutor') {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/tutors" element={<Tutors />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/tutor/dashboard" element={<DashboardTutor />} />
-        <Route path="/tutor/create-profile" element={<CreateProfile />} />
+
+        {/* Protected Tutor Routes */}
+        <Route path="/tutor">
+          <Route path="dashboard" element={
+            <RequireAuth>
+              <RequireTutor>
+                <DashboardTutor />
+              </RequireTutor>
+            </RequireAuth>
+          } />
+          <Route path="create-profile" element={
+            <RequireAuth>
+              <RequireTutor>
+                <CreateProfile />
+              </RequireTutor>
+            </RequireAuth>
+          } />
+          <Route path="edit-profile" element={
+            <RequireAuth>
+              <RequireTutor>
+                <EditProfile />
+              </RequireTutor>
+            </RequireAuth>
+          } />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
