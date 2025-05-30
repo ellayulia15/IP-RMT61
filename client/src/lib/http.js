dd1629d3f0f2a07a9195185e1cc7afe99910b53d
ellayulia15 <ella.yuliacahyani@gmail.com>
2025-05-30 08:05:35 +0700

    fix: update baseURL to production server

--- client/src/lib/http.js
+++ client/src/lib/http.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    // baseURL: 'https://tutorhub-server.cahya.web.id'
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://tutorhub-server.cahya.web.id'
 });
 
 export default http;
